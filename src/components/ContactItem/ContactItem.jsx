0d257b6a9acded2c563add34d9f065591794dfc7
Oleksandr Klyn <shoni0807@gmail.com>
2023-04-27 15:33:53 +0300

    upd

---
+++ src/components/ContactItem/ContactItem.jsx
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types';
+import { Button, Item } from './ContactItem.styled';
+
+export const ContactItem = ({ contacts, onDeleteContact }) => {
+  return contacts.map(({ id, name, number }) => {
+    return (
+      <Item key={id}>
+        {name}: {number}
+        <Button type="button" onClick={() => onDeleteContact(id, name)}>
+          Delete
+        </Button>
+      </Item>
+    );
+  });
+};
+
+ContactItem.propTypes = {
+  contacts: PropTypes.array.isRequired,
+  onDeleteContact: PropTypes.func.isRequired,
+};
---
+++ src/components/ContactsList/ContactsList.styled.jsx
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+
+export const Ol = styled.ol`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  font-size: 20px;
+`;
